9b5b85cd6d9780bdabea1fa56f2094fc54d54e66
James Shaw <js102@zepler.net>
2017-05-30 11:29:46 +0100

    run tests

--- override_dh.sh
+++ override_dh.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 source $FOAM_BASHRC
+export FOAM_USER_APPBIN=$DH_ROOT_DIR/build/bin
+export FOAM_USER_LIBBIN=$DH_ROOT_DIR/build/lib
+export LD_LIBRARY_PATH=$FOAM_USER_LIBBIN:$LD_LIBRARY_PATH
 $1
