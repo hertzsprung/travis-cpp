6474157c3177eb7aacab6316735039dc3130c540
James Shaw <js102@zepler.net>
2017-05-30 08:07:48 +0100

    build testTravisFoam

--- override_dh_auto_build.sh
+++ override_dh_auto_build.sh
@@ -3,4 +3,4 @@ export FOAM_USER_APPBIN=$DH_ROOT_DIR/build/bin
 export FOAM_USER_LIBBIN=$DH_ROOT_DIR/build/lib
 wmake TravisFOAM
 wmake travisFoamApp
-#wmake testTravisFOAM
+wmake testTravisFOAM
--- override_dh_auto_clean.sh
+++ override_dh_auto_clean.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 wclean travisFoamApp
+wclean TravisFOAM
+wclean testTravisFoam
 rm -rf $DH_ROOT_DIR/build/bin
